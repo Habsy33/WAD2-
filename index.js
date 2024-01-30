ea6b392cc5e1a6d7073c16a253dccff8e349a661
Habsy33 <93338971+Habsy33@users.noreply.github.com>
2024-01-30 14:21:48 +0000

    new task

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ http.createServer(function(req, res) {
     res.write('<h1>Welcome to my application</h1>');
     res.write('<div>This response is sent using Node.js</div>');
     res.end('<div>Have a nice day!</div>'); 
-    res.writeHead(200,
-{ 'Content-type': 'text/plain' });
+res.writeHead(200,
+{ 'Content-type': 'text/html' });
  res.end('Hello from my Node.js application!');
 }).listen(3000);
 
