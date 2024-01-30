ecab1aadb70fd1cc60413ecb44bde3742663d586
Habsy33 <93338971+Habsy33@users.noreply.github.com>
2024-01-30 14:24:03 +0000

    changed port

--- homepage.js
+++ homepage.js
@@ -5,17 +5,17 @@ http.createServer(function(req,res){
 var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 switch(path) {
 case '':
-res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.writeHead(200, { 'Content-Type': 'text/html' });
 res.end('Homepage');
 break;
 case '/about':
-res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.writeHead(200, { 'Content-Type': 'text/html' });
 res.end('About');
 break;
 default:
-res.writeHead(404, { 'Content-Type': 'text/plain' });
+res.writeHead(404, { 'Content-Type': 'text/html' });
 res.end('Not Found');
 break;
  }
-}).listen(3000);
+}).listen(8000);
 console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
