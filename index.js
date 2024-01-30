7ab5926d930b923f5f000b77364959e747ea8e2d
Habsy33 <93338971+Habsy33@users.noreply.github.com>
2024-01-30 14:18:49 +0000

    cleaned it up a bit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 http.createServer(function(req, res) {
-res.writeHead(200,
+    res.write('<h1>Welcome to my application</h1>');
+    res.write('<div>This response is sent using Node.js</div>');
+    res.end('<div>Have a nice day!</div>'); 
+    res.writeHead(200,
 { 'Content-type': 'text/plain' });
  res.end('Hello from my Node.js application!');
 }).listen(3000);
+
 console.log('Server started on port 3000, ctrl^c to quit.'); 
